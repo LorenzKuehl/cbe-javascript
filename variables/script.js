f14515b0db218f26dac47965575279e217358a32
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-01-10 10:42:54 +0100

    isString function

--- variables/script.js
+++ variables/script.js
@@ -7,3 +7,24 @@ function getUserNameLength() {
 
 console.log(getUserNameLength(userName) > 4);
 // ^______________ Should log true
+
+// TODO: Implement the isString function
+
+let isString = function (a) {
+  return typeof a === "string";
+};
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
