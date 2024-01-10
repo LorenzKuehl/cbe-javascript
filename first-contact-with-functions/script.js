46952f2e472c14c9b629f60f0e364206924b8c7d
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-01-10 09:26:55 +0100

    Add Positive Function

--- first-contact-with-functions/script.js
+++ first-contact-with-functions/script.js
@@ -17,3 +17,18 @@ console.log(calcGrossPrice(20, 0.19));
 
 console.log(calcGrossPrice(40, 0.16));
 // result should be 46.4
+
+// TODO: Implement the addPositive function
+
+function addPositive(a, b) {
+  return Math.abs(a) + Math.abs(b);
+}
+
+console.log(addPositive(2, 3));
+// result1 should be 5
+
+console.log(addPositive(3, -5));
+// result2 should be 8
+
+console.log(addPositive(-1, -8));
+// result2 should be 9
