eb1dd5242a092057d75937fb2f5d998b0cd172c8
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-01-10 17:18:33 +0100

    oddEven function

--- if-and-switch-statements/script.js
+++ if-and-switch-statements/script.js
@@ -1,4 +1,4 @@
-const size = 25;
+/*const size = 25;
 let result;
 
 if (size > 10 && size < 20) {
@@ -10,4 +10,28 @@ if (size > 10 && size < 20) {
 }
 
 console.log(result);
+*/
+
 // Returns Greater than 10 but should return Greater than 20
+
+let oddEven = function (a) {
+  if (a % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+};
+
+// TODO: Implement the oddEven function
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
