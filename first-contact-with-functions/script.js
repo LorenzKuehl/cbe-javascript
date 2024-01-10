83b034109700b66aa17b66bf3854025fb8b4c4d8
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-01-10 09:17:56 +0100

    Gross Price Function

--- first-contact-with-functions/script.js
+++ first-contact-with-functions/script.js
@@ -7,3 +7,13 @@ console.log(welcomeMsg("Jane"));
 
 console.log(welcomeMsg("Marc"));
 // Welcome Marc!
+
+// TODO: implement calcGrossPrice function
+function calcGrossPrice(a, b) {
+  return a + a * b;
+}
+console.log(calcGrossPrice(20, 0.19));
+// result should be 23.8
+
+console.log(calcGrossPrice(40, 0.16));
+// result should be 46.4
