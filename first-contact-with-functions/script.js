9a76c3f6deb11bef9364528ac531b3a22418a203
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-01-10 09:14:59 +0100

    Greet Function

---
+++ first-contact-with-functions/script.js
@@ -0,0 +1,9 @@
+// TODO: implement welcomeMsg function
+function welcomeMsg(a) {
+  return "Welcome " + a + "!";
+}
+console.log(welcomeMsg("Jane"));
+// Welcome Jane!
+
+console.log(welcomeMsg("Marc"));
+// Welcome Marc!
